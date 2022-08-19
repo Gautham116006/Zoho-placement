bf2294b05e777a5d1d8190030087a1a5ca62effb
Gautham116006 <63862992+Gautham116006@users.noreply.github.com>
2022-08-19 17:09:20 +0530

    Initial commit

---
+++ zoho 2d array search.py
@@ -0,0 +1,27 @@
+'''
+search for substring in WELCOMETOZOHOCORPORATION in 2d array
+return start and end index
+'''
+
+base_arr = [['w', 'e', 'l', 'c', 'o'], ['m', 'e', 't', 'o', 'z'], ['o', 'h', 'o', 'c', 'o'], ['r', 'p', 'o', 'r', 'a'],
+            ['t', 'i', 'o', 'n', ' ']]
+
+key = 'too'
+word = ""
+count = 0
+for i in range(5):
+    for j in range(5):
+        if base_arr[i][j] == key[count]:
+            for k in range():
+
+
+
+
+
+
+
+
+
+
+
+
